8fdbd5f34e6650875aa6846fcaab9dec33a4eacf
TrimovaAisha <tfnbhhahaha@gmail.com>
2026-03-30 16:00:18 +0500

    fix cors for vercel frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
+import Card from './pages/Card'
 
 function App() {
   return (
